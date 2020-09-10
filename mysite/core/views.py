532cc78ce0c66d8474f6b8e730ce03f97ec3b921
Hongbin Lin <hongbinlin@Hongbins-MacBook-Air.local>
2020-09-10 16:39:51 -0700

    update

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
@@ -16,3 +19,10 @@ def signup(request):
     else:# GET request
         form = UserCreationForm()
     return render(request,'registration/signup.html',{'form':form})
+
+@login_required
+def secret_page(request):
+    return render(request,'secret_page.html')
+
+class SecretPage(LoginRequiredMixin,TemplateView):
+    template_name = 'secret_page.html'
